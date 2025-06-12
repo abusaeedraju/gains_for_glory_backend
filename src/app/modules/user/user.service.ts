e48e835fce7ce34ef259c892404dc5a1fccbef81
unknown <rtraju2016@gmail.com>
2025-06-12 19:56:06 +0600

    first commit

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,134 @@
+import { User } from "@prisma/client";
+import ApiError from "../../error/ApiErrors";
+import { StatusCodes } from "http-status-codes";
+import { compare, hash } from "bcrypt"
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { OTPFn } from "../../helper/OTPFn";
+import OTPVerify from "../../helper/OTPVerify";
+import { getImageUrl } from "../../helper/uploadFile";
+import { prisma } from "../../../utils/prisma";
+import { jwtHelpers } from "../../helper/jwtHelper";
+
+const createUserIntoDB = async (payload: any) => {
+
+    const findUser = await prisma.user.findUnique({
+        where: {
+            email: payload.email
+        }
+    })
+    if (findUser && findUser?.isVerified) {
+        throw new ApiError(StatusCodes.NOT_FOUND, "User already exists")
+    }
+    if (findUser && !findUser?.isVerified) {
+        await OTPFn(payload.email)
+        return
+    }
+    if (payload.password !== payload.confirmPassword) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, "Password and confirm password does not match")
+    }
+    const { confirmPassword, ...filteredPayload } = payload
+    const newPass = await hash(filteredPayload.password, 10)
+
+    const result = await prisma.user.create({
+        data: {
+            ...filteredPayload,
+            password: newPass,
+        },
+        select: {
+            id: true,
+            email: true,
+            role: true,
+            status: true,
+            createdAt: true
+        }
+    })
+    OTPFn(payload.email)
+    return result
+}
+
+const changePasswordIntoDB = async (id: string, payload: any) => {
+
+    const findUser = await prisma.user.findUnique({
+        where: {
+            id
+        }
+    })
+    if (!findUser) {
+        throw new ApiError(StatusCodes.NOT_FOUND, "User not found")
+    }
+    try {
+        const isMatch = await compare(payload.oldPassword, findUser.password)
+        if (!isMatch) {
+            throw new ApiError(StatusCodes.BAD_REQUEST, "Old password is incorrect")
+        }
+        const newPass = await hash(payload.newPassword, 10)
+        await prisma.user.update({
+            where: {
+                id
+            },
+            data: {
+                password: newPass
+            }
+        })
+        return
+
+    } catch {
+        throw new ApiError(StatusCodes.BAD_REQUEST, "Old password is incorrect")
+    }
+}
+
+const updateUserIntoDB = async (id: string, payload: any, image: any) => {
+
+
+    const userImage = await getImageUrl(image)
+
+    try {
+        const result = await prisma.user.update({
+            where: {
+                id
+            },
+            data: {
+                ...payload,
+                image: userImage ?? undefined
+            }
+        })
+        const updateDetails = {
+            id: result.id,
+            name: result.name,
+            email: result.email,
+            image: result.image,
+            role: result.role,
+            createdAt: result.createdAt,
+            updatedAt: result.updatedAt,
+        }
+
+        return updateDetails
+
+    } catch (error) {
+        throw new ApiError(StatusCodes.BAD_REQUEST, "User not found")
+    }
+}
+
+
+const getMyProfile = async (id: string) => {
+
+    const result = await prisma.user.findUnique({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            role: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    })
+
+    return result
+}
+
+
+export const userServices = { createUserIntoDB, updateUserIntoDB, changePasswordIntoDB, getMyProfile }
---
+++ src/app/route/route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import { userRoutes } from "../modules/user/user.routes"
+import { authRoutes } from "../modules/auth/auth.routes"
+
+const router = Router()
+const routes = [
+    {
+        path: "/user",
+        component: userRoutes
+    },
+    {
+        path: "/auth",
+        component: authRoutes
+    },
+]
+
+routes.forEach(route => router.use(route.path, route.component))
+export default router 
