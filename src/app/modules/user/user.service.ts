276598f314b3813553dff14eba7f0d5dc7359db4
unknown <rtraju2016@gmail.com>
2025-06-27 11:48:58 +0600

    make changes in user module and model

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -34,6 +34,7 @@ const createUserIntoDB = async (payload: any) => {
         },
         select: {
             id: true,
+            name: true,
             email: true,
             role: true,
             status: true,
