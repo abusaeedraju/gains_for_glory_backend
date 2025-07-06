98eebd614960c893e8bddf1654c50794455af919
Raju <raju@example.com>
2025-07-06 17:47:56 +0600

    added modification

--- src/app/modules/cart/cart.service.ts
+++ src/app/modules/cart/cart.service.ts
@@ -2,21 +2,26 @@ import { prisma } from "../../../utils/prisma"
 import ApiError from "../../error/ApiErrors"
 import { StatusCodes } from "http-status-codes"
 
-const addToCart = async (payload: any, userId: string,productId:string) => {
-    const result = await prisma.cart.create({ data: { ...payload, userId,productId } })
+const addToCart = async (payload: any, userId: string, productId: string) => {
+    const result = await prisma.cart.create({
+        data: {
+            ...payload,
+            userId, productId
+        }
+    })
     return result
 }
 const getMyCartItem = async (userId: string) => {
 
     const result = await prisma.cart.findMany({
         where: { userId },
-        include:{
-            productDetails:{
-                select:{
-                    id:true,
-                    name:true,
-                    price:true,
-                    image:true,
+        include: {
+            productDetails: {
+                select: {
+                    id: true,
+                    name: true,
+                    price: true,
+                    image: true,
                 }
             }
         }
@@ -35,11 +40,11 @@ const updateCartItem = async (cartId: string, payload: any) => {
     const cart = await prisma.cart.findUnique({ where: { id: cartId } })
     if (!cart) {
         throw new ApiError(StatusCodes.NOT_FOUND, "Cart not found")
-    }   
+    }
     const result = await prisma.cart.update({ where: { id: cartId }, data: payload })
     return result
 }
 
 export const cartService = {
-    addToCart, getMyCartItem, deleteCartItem , updateCartItem
+    addToCart, getMyCartItem, deleteCartItem, updateCartItem
 }
